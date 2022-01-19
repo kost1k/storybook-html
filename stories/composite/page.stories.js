bf20d0a83d2c8309328629ca163db8544f8a02d6
kost1k <kostik048@gmail.com>
2022-01-19 13:21:32 +0300

    Add files via upload

---
+++ stories/composite/page.stories.js
@@ -0,0 +1,24 @@
+import { createPage } from './page';
+import * as HeaderStories from './header.stories';
+
+export default {
+  title: 'Composite/Page',
+  argTypes: {
+    onLogin: { action: 'onLogin' },
+    onLogout: { action: 'onLogout' },
+    onCreateAccount: { action: 'onCreateAccount' },
+  },
+};
+
+const Template = (args) => createPage(args);
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  // More on composing args: https://storybook.js.org/docs/html/writing-stories/args#args-composition
+  ...HeaderStories.LoggedIn.args,
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  ...HeaderStories.LoggedOut.args,
+};
